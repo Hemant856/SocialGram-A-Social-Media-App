00ef25fcbf28fda7f142b94510ecd0ff832e86c3
Hemant856 <rajhemant3276@gmail.com>
2021-09-13 12:33:43 +0530

    add a controller and a router for home

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+
+// use express router
+app.use('/', require('./routes'));
+
 app.listen(port, function(err){
      
     if(err){
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+// Entry Point of all the routes
+
+const express = require('express');
+
+const router = express.Router();
+const homeController = require('../controllers/home_controller');
+
+//console.log('Router loaded');
+
+router.get('/', homeController.home);
+
+module.exports = router;
