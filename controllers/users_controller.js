b978e760d2b73077ec681c99960496c716cf6c18
Hemant856 <rajhemant3276@gmail.com>
2021-09-30 23:49:19 +0530

    Create session

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -50,5 +50,33 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for user 
 module.exports.createSession = function(req, res){
-  // TODo later
+  // Find the User 
+   User.findOne({email: req.body.email}, function(err, user){
+    if(err){console.log('error in finding user in signing in'); return}
+   
+     //handle user found
+     if(user){
+       //handle password which doesn't match
+       if(user.password != req.body.password){
+          return res.redirect('back');
+       }
+
+       // handle session creation
+       res.cookie('user_id', user.id);
+       return res.redirect('/users/profile');
+
+     }
+     else{
+        //handle user not found
+        return res.redirect('back');
+     }
+    
+
+   });
+
+ 
+  
+
+  
+
 }
