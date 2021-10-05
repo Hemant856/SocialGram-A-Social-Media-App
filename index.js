6a2e23d836e375dc8c27b499d0c6d9856fd2be86
Hemant856 <rajhemant3276@gmail.com>
2021-10-05 17:26:09 +0530

    add scss

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src: './assets/scss',
+    dest: './assets/css',
+    debug: true,
+    outputStyle: 'expanded',
+    prefix: '/css'
+}))
+
+
 
 app.use(express.urlencoded());
 app.use(cookieParser());
