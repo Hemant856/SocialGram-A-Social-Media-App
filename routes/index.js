37c85afd1f077521c26c4b2bd3bda133a77da2cb
Hemant856 <rajhemant3276@gmail.com>
2021-11-26 15:25:35 +0530

    V1 api

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,8 @@ router.use('/users', require('./users'));
 
 router.use('/posts', require('./posts'));
 router.use('/comments',require('./comments'));
+
+router.use('/api', require('./api'));
+
+
 module.exports = router;
