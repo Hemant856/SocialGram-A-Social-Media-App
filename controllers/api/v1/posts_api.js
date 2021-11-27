73ef32dab0e44867c1cd3e69ac9814ba89894ce3
Hemant856 <rajhemant3276@gmail.com>
2021-11-27 11:32:08 +0530

    JWT

---
+++ controllers/api/v1/posts_api.js
@@ -0,0 +1,45 @@
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function(req, res){
+
+    let posts = await Post.find({})
+    .sort('-createdAt')
+      .populate('user')
+      .populate({
+        path: 'comments',
+        populate: {
+          path: 'user'
+        }
+      });
+
+
+
+    return res.json(200, {
+        message: "List of posts",
+        posts: posts
+    })
+}
+
+module.exports.destroy = async function (req, res) {
+
+
+    try {
+        let post = await Post.findById(req.params.id);
+
+        //.id means converting the object id into string
+       // if (post.user == req.user.id) {
+            post.remove();
+
+            await Comment.deleteMany({ post: req.params.id });
+            
+            return res.json(200, {
+                message: "Post and Associated comments Deleted Successfully"
+            })
+
+        } catch (err) {
+            return res.json(500, {
+                message: "Internal Server Error"
+            });
+        }
+    }
